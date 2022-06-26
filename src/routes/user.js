a65a0d6c605c790146e40ddfb4498e6c53d8855a
nithinpkinfo <nithinpkinfo@gmail.com>
2022-06-26 08:49:23 +0530

    update and delete user

--- src/routes/user.js
+++ src/routes/user.js
@@ -8,5 +8,7 @@ User.post("/login", controller.login);
 User.post("/signup", controller.signup);
 User.get("/list", auth, controller.list);
 User.get("/list/:id", auth, controller.listById);
+User.put("/:id", auth, controller.update);
+User.delete("/:id", auth, controller.delete);
 
 module.exports = User;
